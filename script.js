7fa76162671c1cac5da24160196c97033a64f911
Artur Papyan <angry.root@gmail.com>
2023-12-22 13:27:51 +0400

    display the extracted maxres image and give a download button

--- script.js
+++ script.js
@@ -3,10 +3,25 @@ document.getElementById('youtubeForm').addEventListener('submit', function(event
 
     var youtubeLink = document.getElementById('youtubeLink').value;
     var videoId = extractYoutubeVideoId(youtubeLink);
-    var imageUrl = 'https://img.youtube.com/vi/' + videoId + '/maxresdefault.jpg'; // Updated to use maxresdefault.jpg
+    var imageUrl = 'https://img.youtube.com/vi/' + videoId + '/maxresdefault.jpg';
 
     document.getElementById('videoIdOutput').innerText = 'Video ID: ' + videoId;
     document.getElementById('imageUrlOutput').innerText = 'Image URL: ' + imageUrl;
+
+    var imageElement = document.getElementById('youtubeImage');
+    var downloadLink = document.getElementById('downloadLink');
+
+    if (videoId !== 'Invalid URL') {
+        imageElement.src = imageUrl;
+        imageElement.style.display = 'block';
+
+        downloadLink.href = imageUrl;
+        downloadLink.download = videoId + '_maxresdefault.jpg';
+        downloadLink.style.display = 'block';
+    } else {
+        imageElement.style.display = 'none';
+        downloadLink.style.display = 'none';
+    }
 });
 
 function extractYoutubeVideoId(url) {
