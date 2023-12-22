1c46697633fd9b52512886eba00551775f9a7a59
Artur Papyan <angry.root@gmail.com>
2023-12-22 13:25:49 +0400

    Display the hi rez thumbnail url

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ document.getElementById('youtubeForm').addEventListener('submit', function(event
 
     var youtubeLink = document.getElementById('youtubeLink').value;
     var videoId = extractYoutubeVideoId(youtubeLink);
+    var imageUrl = 'https://img.youtube.com/vi/' + videoId + '/maxresdefault.jpg'; // Updated to use maxresdefault.jpg
 
     document.getElementById('videoIdOutput').innerText = 'Video ID: ' + videoId;
+    document.getElementById('imageUrlOutput').innerText = 'Image URL: ' + imageUrl;
 });
 
 function extractYoutubeVideoId(url) {
